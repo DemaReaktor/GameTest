8253f6f824deb179eb1b8242d026d3d238515ed7
Dema <67798322+DemaReaktor@users.noreply.github.com>
2022-02-05 18:59:13 +0200

    Menu start
    Created scene Menu. Added 5 buttons. Also created class AutoPanel for automatically setting scales and positions of buttons.

---
+++ N111(GameTest2)/Assets/Scripts/AutoPanel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoPanel : MonoBehaviour
+{
+    void Awake()
+    {
+        Normalize();
+    }
+    private void Normalize()
+    {
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+        if (transform.GetComponent<RectTransform>() != null)
+        {
+            transform.GetComponent<RectTransform>().position = new Vector2(
+                (int)((float)transform.GetComponent<RectTransform>().position.x * screenSize.x / config.standartScreenSize.x),
+                (int)((float)transform.GetComponent<RectTransform>().position.y * screenSize.y / config.standartScreenSize.y));
+            transform.GetComponent<RectTransform>().localScale = new Vector2((float)screenSize.x / config.standartScreenSize.x,
+                (float)screenSize.y / config.standartScreenSize.y);
+            if (transform.GetComponent<Text>() != null)
+                transform.GetComponent<Text>().fontSize = (int)((float)transform.GetComponent<Text>().fontSize * minimumChange(screenSize));
+        }
+    }
+    private float minimumChange(Vector2 screenSize)
+    {
+        float changeX = screenSize.x / config.standartScreenSize.x;
+        float changeY = screenSize.y / config.standartScreenSize.y;
+        return changeX < changeY ? changeX : changeY;
+    }
+}
---
+++ N111(GameTest2)/Assets/Scripts/config.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class config
+{
+    public static Vector2 standartScreenSize => new Vector2(1920, 1080);                
+}
